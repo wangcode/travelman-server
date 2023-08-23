a90cddde0d2e4475aa6aaf5b1b85df1f767ed867
Wang <wang@liujia.wang>
2023-08-24 01:39:44 +0800

    update prisma import way.

--- src/apps/app.module.ts
+++ src/apps/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '@apps/user/user.module';
 import { AuthModule } from '@apps/auth/auth.module';
-import { PrismaService } from '@common/prisma/prisma.service';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
